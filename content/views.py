67ddd13445317d1d9c9d05dbb6a231144f51a6ef
Andre Scherma Soleo <andreschermas@gmail.com>
2017-07-19 04:27:17 -0300

    templates and views functions included

---
+++ content/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def processes(request):
+	return render(request, 'content/processes.jade')
+
+def works(request):
+	return render(request, 'content/works.jade')
+
+def serie(request, serie_id):
+	return render(request, 'content/serie.jade', context)
+
+def view_work (request, work_id):
+	return render(request, 'content/serie.jade', context)
+
