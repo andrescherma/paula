27d51e39922d04d8e7169412fa10773c95c79a33
André Scherma <andrescherma@Ricardos-Mac-mini.local>
2017-07-27 16:50:05 -0300

    working in static files config

--- paula/urls.py
+++ paula/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
+
 from django.contrib import admin
+
 import views
 
 urlpatterns = [
@@ -24,4 +28,6 @@ urlpatterns = [
     url(r'^', include('content.urls', namespace='content')),
 
     url(r'^admin/', admin.site.urls), 
-]
+    ] + [
+        static(settings.STATIC_URL, document_root=settings.BASE_DIR),
+    ]
