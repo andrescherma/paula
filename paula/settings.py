7e87ed12e7e49e9f9a0aae9bb4ba110b29ff5776
Andre Scherma Soleo <andreschermas@gmail.com>
2017-07-30 18:19:14 -0300

    db settings for developments

--- paula/settings.py
+++ paula/settings.py
@@ -89,11 +89,14 @@ WSGI_APPLICATION = 'paula.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'paula_db',
+        'USER': 'paula',
+        'PASSWORD': 'admin',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -133,5 +136,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_URL = 'static/'
 
-STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "/static"),
+]
